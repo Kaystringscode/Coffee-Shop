296c4419a96ff12ac1daccca004dd9f7a5b61ed1
kaystringscode <kaystrings@gmail.com>
2023-04-14 20:14:26 +0100

    creating banners

---
+++ components/banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+	return (
+		<div>
+			<h1>
+				<span>Coffee </span> <span>Connoisseur</span>
+			</h1>
+			<p>Discover Your local Coffee Shops☕️</p>
+			<button>Veiw Stores nearby </button>
+		</div>
+	);
+};
+export default Banner;
