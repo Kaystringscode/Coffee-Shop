0628b8a9982d79c6e3780f48a47da9d920c69409
kaystringscode <kaystrings@gmail.com>
2023-04-14 22:01:17 +0100

    back to home link created

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,14 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const coffeeStore = () => {
 	const router = useRouter();
 	console.log("router", router);
-	return <div>Coffee Store Page</div>;
+	return (
+		<div>
+			Coffee Store Page {router.query.id}
+			<Link href="/">Back To Home</Link>
+		</div>
+	);
 };
 export default coffeeStore;
